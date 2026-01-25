22fd5132a56c9dbf474943175db556f87a7e5029
MrChayras5 <diegocandaneda38@gmail.com>
2026-01-25 15:47:16 -0600

    Estructura basica de los msv, pendiente api.gateway

---
+++ msv-huesped/src/main/java/com/huesped/MsvHuespedApplication.java
@@ -0,0 +1,13 @@
+package com.huesped;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvHuespedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvHuespedApplication.class, args);
+	}
+
+}
